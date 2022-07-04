5669335fc51cdbc788ddd8880907ca896df7fdbe
LorenzoVeropalumbo <lorenzo-veropalumbo@outlook.it>
2022-07-04 13:04:47 +0200

    end exercise

--- js/script.js
+++ js/script.js
@@ -7,10 +7,13 @@ var app = new Vue(
       },
       methods: {
         getRandomEmailFromApi() {
+          // save all email in the array
           for (let i = 10; i > this.emailList.length; i--) {
+            // HTTP call to take email
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
               this.randomEmail = response.data.response;
+              // push email in the array
               this.emailList.push(this.randomEmail);
             });       
           }              
